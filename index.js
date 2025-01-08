41d69d1e5e4cf6a365661df661ed31b999814253
dekalouis <dekarebaudo@gmail.com>
2025-01-08 19:17:53 +0700

    Menyelesaikan kerangka HTML dan memulai function addExpense()

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 // buat fungsionalitas ---
-//1. bikin fungsi tambah pengeluaran (reset)
+// Database awal untuk pengetesan
 
-//2. function render buat displaynya update
+let expenses = [
+  {
+    id: Date.now() + 1,
+    name: "Belanja bulanan",
+    amount: 1500000,
+    date: "2025-01-01",
+    category: "Kebutuhan Pokok",
+  },
+  {
+    id: Date.now() + 2,
+    name: "Gojek ke kantor",
+    amount: 35500,
+    date: "2025-01-01",
+    category: "Transportasi",
+  },
+];
 
-//3. bikin CRUDnya buat add/edit/delete dll2
+const formPengeluaran = document.getElementById("form-pengeluaran");
 
-//4. bikin function filternya
+//1. bikin fungsi tambah pengeluaran (reset) - Deka
+function addPengeluaran() {
+  const name = document.getElementById("nama-pengeluaran").value;
+  const amount = Number(document.getElementById("jumlah-pengeluaran").value);
+  const date = document.getElementById("tanggal-pengeluaran").value;
+  const category = document.getElementById("kategori-pengeluaran").value;
+  //   console.log(name, amount, date, category);
+  if (!name || !amount || !category || !date) {
+    alert(`Isi dulu yuk datanya!`);
+  }
 
-//5. yang styling coba dr luar yaaa ekekkekek
+  let pengeluaran = {
+    id: Date.now(),
+    name,
+    amount,
+    category,
+    date,
+  };
+
+  console.log(pengeluaran);
+  expenses.push(pengeluaran);
+  formPengeluaran.reset();
+}
+
+//2. function render buat displaynya update - Rian
+
+//3. bikin CRUDnya buat add/edit/delete dll2 - Thomy
+
+//4. bikin function filternya - Raden
+
+//5. yang styling coba dr luar yaaa ekekkekek - Resya
