4979387444048743f1bb2f7a77a29ee7d2854429
dekalouis <dekarebaudo@gmail.com>
2025-01-08 15:49:12 +0700

    inital commit untuk file index.html&js beserta style.ss kosong

---
+++ index.js
@@ -0,0 +1,10 @@
+// buat fungsionalitas ---
+//1. bikin fungsi tambah pengeluaran (reset)
+
+//2. function render buat displaynya update
+
+//3. bikin CRUDnya buat add/edit/delete dll2
+
+//4. bikin function filternya
+
+//5. yang styling coba dr luar yaaa ekekkekek
